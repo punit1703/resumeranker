378325f8e4bc77f0c501f3594265dcc5c211aaa9
Punit Patel <pr17032006@gmail.com>
2026-01-13 13:44:32 +0530

    changed Generator.jsx

--- frontend/pages/Generate.jsx
+++ frontend/pages/Generate.jsx
@@ -40,8 +40,8 @@ function Textarea({ icon: Icon, label, ...props }) {
 }
 
 export default function Generate() {
-  // const API_BASE = "https://resume-ranker-backend-esei.onrender.com";
-  const API_BASE = "http://127.0.0.1:8000/";
+  const API_BASE = "https://resume-ranker-backend-esei.onrender.com";
+  // const API_BASE = "http://127.0.0.1:8000/";
   const [form, setForm] = useState({
     name: "",
     email: "",
